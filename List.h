52dae48438262e84f7d3fa715ccb59e19438d2d9
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-12 18:39:43 +0300

    created declarations

---
+++ List.h
@@ -0,0 +1,16 @@
+//
+// Created by Ann Nikolaeff on 12.10.2022.
+//
+#include <stdbool.h>
+#ifndef LINKED_LIST_TEAM_2_LIST_H
+#define LINKED_LIST_TEAM_2_LIST_H
+typedef struct Node{
+    
+}Node;
+int addHead(Node **node);
+int add(Node **head);
+int delete(Node **node);
+int deleteHead(Node **head);
+bool isEmpty(Node **head);
+int clear(Node **head);
+#endif //LINKED_LIST_TEAM_2_LIST_H
