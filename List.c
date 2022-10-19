14a812fd84b161548fc28456cd58c68d7483978b
GitRadium <gidradiumcode@gmail.com>
2022-10-19 18:52:04 +0300

    Add list struct

--- List.c
+++ List.c
@@ -1,1 +1,12 @@
 #include "List.h"
+
+typedef struct ListElement {
+    ListValue value;
+    ListElement *next;
+} ListElement;
+
+typedef struct List {
+    ListElement* head;
+    ListElement* tail;
+    size_t size;
+} List;
